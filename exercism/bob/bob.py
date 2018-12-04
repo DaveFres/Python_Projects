3735040a555e569decd0f1eaea5b98b189bc4134
DaveFres <nanyandave@gmail.com>
2018-12-04 12:17:57 +0300

    Optimized the bob.py program

--- exercism/bob/bob.py
+++ exercism/bob/bob.py
@@ -1,15 +1,14 @@
 def hey(phrase):
+
+    phrase = phrase.rstrip()
+
     if phrase == '':
         return 'Fine. Be that way!'
 
-    try:
-        if phrase.rstrip()[len(phrase.rstrip()) - 1] == '?':
-            if str.isupper(phrase.rstrip()):
-                return 'Calm down, I know what I\'m doing!'
-            return 'Sure.'
-
-    except IndexError:
-        return 'Fine. Be that way!'
+    if phrase[-1] == '?':
+        if str.isupper(phrase.rstrip()):
+            return 'Calm down, I know what I\'m doing!'
+        return 'Sure.'
 
     if str.isupper(phrase):
         return 'Whoa, chill out!'
